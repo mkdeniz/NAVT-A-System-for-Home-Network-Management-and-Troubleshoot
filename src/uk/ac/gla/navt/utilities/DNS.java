4ab257c19f1c6823568ebb0df159c7c66a085ce3
Mehmet Kemal Deniz <mkdeniz@users.noreply.github.com>
2014-03-27 20:16:46 +0000

    Update DNS.java

--- src/uk/ac/gla/navt/utilities/DNS.java
+++ src/uk/ac/gla/navt/utilities/DNS.java
@@ -1,25 +1,42 @@
 package uk.ac.gla.navt.utilities;
 
-import java.io.*;
-import java.util.*;
+/**
+* A program static class to provide UNIX ping tool
+* 
+* @Author Mehmet Kemal Deniz
+* @Date 27/03/2014
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class DNS {
     
     static Process process;
     
+    /**
+     * 
+     * Default Constructor
+     * 
+     * @param h address of host
+     * @param t number of requests
+     * 
+     */
     public static String runTest(String h, String t){
         try {
-            List<String> commands = new ArrayList<>();
+            //Create commnad according to the host and the number of tries
+            ArrayList<String> commands = new ArrayList<>();
             commands.add("/bin/ping");
             commands.add("-c");
             commands.add(t);
             commands.add(h);
             ProcessBuilder pb = new ProcessBuilder(commands);
             
+            //Start Proccess and proccess utput
             process = pb.start();
-       
             BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            
             String s;
             String results;
             while ((s = stdInput.readLine()) != null) {
