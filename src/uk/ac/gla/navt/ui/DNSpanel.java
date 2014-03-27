cab085f5fd7710b420a33bff836656b2b0f384e8
root <root@Acer>
2014-03-27 18:11:10 +0000

    "Almost There"

--- src/uk/ac/gla/navt/ui/DNSpanel.java
+++ src/uk/ac/gla/navt/ui/DNSpanel.java
@@ -75,8 +75,7 @@ public class DNSpanel extends JPanel implements ActionListener  {
         protected JLabel l3 = new JLabel();
         
         protected String green = "images/green.png";
-        protected String red = "/home/mkdeniz/Dropbox/University/"
-                            + "Year4/Project/Images/red.png";
+        protected String red = "images/red.png";
         
         public MonitorWorker(){}
         
@@ -97,11 +96,15 @@ public class DNSpanel extends JPanel implements ActionListener  {
                 t2 = t.split(",");
                 String[] result = t2[1].toString().split("");
                 int recv = Integer.parseInt(result[2]);
+                
+                //Problem
                 if (recv == 0){
                     ImageIcon img1 = new ImageIcon(red);
                     l = new JLabel("\nGoogle DNS");
                     l.setIcon(img1);
                 }
+                
+                //Success
                 else {
                     ImageIcon img1 = new ImageIcon(green);
                     l = new JLabel("\nGoogle DNS");
@@ -142,6 +145,7 @@ public class DNSpanel extends JPanel implements ActionListener  {
             if (suc > 0) {
                 result = suc;    
                 t1 = DNS.runTest("www.google.com","3");
+                //No Errors
                 if (t1 != null) {
                     t2 = t1.split(",");
                     l2 = new JLabel(t2[2]);
@@ -149,12 +153,16 @@ public class DNSpanel extends JPanel implements ActionListener  {
                     l3 = new JLabel("Internet Connection");
                     l3.setIcon(img3);
                 }
+                
+                //ERROR
                 else {
                     ImageIcon img3 = new ImageIcon(red);
                     l3 = new JLabel("Wrong DNS");
                     l3.setIcon(img3);
                 }
             }
+            
+            //No Internet
             else {
                 ImageIcon img3 = new ImageIcon(red);
                 l3 = new JLabel("You have no Internet Connection. Contact your ISP");
