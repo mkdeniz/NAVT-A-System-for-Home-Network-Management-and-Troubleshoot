9fdcfdbdda35c6604f0f85c7db3dbd7215981f6c
Mehmet Kemal Deniz <mkdeniz@users.noreply.github.com>
2014-03-27 20:28:17 +0000

    Update Database.java

--- src/uk/ac/gla/navt/utilities/Database.java
+++ src/uk/ac/gla/navt/utilities/Database.java
@@ -1,10 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package uk.ac.gla.navt.utilities;
+
+/**
+* A class to proivde database and graph creation
+* 
+* @Author Mehmet Kemal Deniz
+* @Date 27/03/2014
+*/
 
 import java.awt.Color;
 import java.io.IOException;
@@ -17,27 +18,35 @@ import org.jrobin.core.Sample;
 import org.jrobin.core.Util;
 import org.jrobin.graph.RrdGraphDef;
 
-/**
- *
- * @author root
- */
 public class Database {
     
-    protected String file;
-    
-    public Database(String f) {
-        file = f;
+    /**
+     *
+     * Default Constructor
+     * 
+     */
+    public Database() {
     }
     
+    /**
+     *
+     * A method to create bandwidth database
+     * 
+     * */
     public void prepareRRDB() throws IOException, RrdException {
-        RrdDef rrdDef = new RrdDef(this.file, 5);
-        rrdDef.addDatasource("a", "GAUGE", 2, Double.NaN, Double.NaN);
-        rrdDef.addDatasource("b", "GAUGE", 2, Double.NaN, Double.NaN);
+        RrdDef rrdDef = new RrdDef("file.rrd", 5);
+        rrdDef.addDatasource("download", "GAUGE", 2, Double.NaN, Double.NaN);
+        rrdDef.addDatasource("upload", "GAUGE", 2, Double.NaN, Double.NaN);
         rrdDef.addArchive("AVERAGE", 0.5, 1, 600);
         RrdDb rrdDb = new RrdDb(rrdDef, RrdBackendFactory.getFactory("MEMORY"));
         rrdDb.close();
     }
     
+    /**
+     *
+     * A method to create classification database
+     * 
+     * */
     public void prepareRRDC() throws IOException, RrdException {
         RrdDef rrdDef = new RrdDef("classification.rrd");
         rrdDef.addDatasource("WEB", "GAUGE", 600, Double.NaN, Double.NaN);
@@ -49,23 +58,11 @@ public class Database {
         rrdDb.close();
     }
     
-    public void test(int N) throws IOException, RrdException, InterruptedException {
-        RrdDb rrdDb = new RrdDb("classification.rrd");
-        Sample sample = rrdDb.createSample();
-        for (int i = 1; i<N; i++){
-            sample.setTime(Util.getTimestamp());
-                    sample.setValue("WEB", i);
-                    if (N % 2 == 0){
-                    sample.setValue("BULK", i);}
-                    if (N > 30){
-                    sample.setValue("P2P", i);}
-                    sample.setValue("SERVICE", i*2);
-                    sample.update();
-                    Thread.sleep(1200);
-        }
-        rrdDb.close();
-    }
-    
+    /**
+     *
+     * A method to create bandwidth graph
+     * 
+     * */
     public RrdGraphDef prepareBandwith() throws RrdException {
         RrdGraphDef gDef = new RrdGraphDef();
         Date endTime = new Date();
@@ -75,16 +72,17 @@ public class Database {
         gDef.setTitle("Bandiwth");
         gDef.setVerticalLabel("Speed");
         gDef.setTimeAxisLabel("Time");
-        gDef.datasource("a", file, "a", "AVERAGE", "MEMORY");
-        gDef.area("a", Color.RED, "Download");
-        gDef.gprint("a", "MAX", "max = @2 kb/s");
-        gDef.gprint("a", "AVERAGE", "avg = @2 kb/s@l");
-        gDef.datasource("b", file, "b", "AVERAGE", "MEMORY");
-        gDef.area("b", Color.GREEN, "Upload");
-        gDef.gprint("b", "MAX", "max = @2 kb/s");
-        gDef.gprint("b", "AVERAGE", "avg = @2 kb/s@l");
+        gDef.datasource("download", "file.rrd", "download", "AVERAGE", "MEMORY");
+        gDef.area("download", Color.RED, "Download");
+        gDef.gprint("download", "MAX", "max = @2 kb/s");
+        gDef.gprint("download", "AVERAGE", "avg = @2 kb/s@l");
+        gDef.datasource("upload", "file.rrd", "upload", "AVERAGE", "MEMORY");
+        gDef.area("upload", Color.GREEN, "Upload");
+        gDef.gprint("uploa", "MAX", "max = @2 kb/s");
+        gDef.gprint("uload", "AVERAGE", "avg = @2 kb/s@l");
         gDef.time("@l@lTime period: @t", "MMM dd, yyyy    HH:mm:ss", startTime);
         gDef.time("to  @t@c", "HH:mm:ss");
+        gDef.close();
         return gDef;
     }
 }
