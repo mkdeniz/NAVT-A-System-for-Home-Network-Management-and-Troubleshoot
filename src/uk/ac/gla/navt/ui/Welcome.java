e3230a0a1a5304f950afbeebc9d39492d724fd15
Mehmet Kemal Deniz <mkdeniz@users.noreply.github.com>
2014-03-27 20:07:56 +0000

    Update Welcome.java

--- src/uk/ac/gla/navt/ui/Welcome.java
+++ src/uk/ac/gla/navt/ui/Welcome.java
@@ -1,25 +1,31 @@
 package uk.ac.gla.navt.ui;
 
+/**
+* A dialog to get NIC from user.
+* 
+* @Author Mehmet Kemal Deniz
+* @Date 27/03/2014
+*/
+
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JTextField;
 import org.jnetpcap.Pcap;
 import org.jnetpcap.PcapIf;
+import org.jrobin.core.RrdException;
 
-/**
- *
- * @author root
- */
 public class Welcome extends JDialog implements ActionListener{
     
-    protected static final String rrd = "file.rrd";
     protected JComboBox List;
     protected JButton button;
     protected JTextField field;
@@ -29,24 +35,30 @@ public class Welcome extends JDialog implements ActionListener{
     protected Vector<String> deviceList;
     protected StringBuilder errbuf;
     
-    
+    /**
+     * 
+     * Default Constructor
+     * 
+     */
     public Welcome(){
         super();
+        //Network Devices
         alldevs = new ArrayList<>();
         errbuf = new StringBuilder();
         Pcap.findAllDevs(alldevs, errbuf); 
-        
         deviceList = new Vector<>();
         for (PcapIf device : alldevs) {
             deviceList.add(device.getName());
         }  
         List = new JComboBox((Vector) deviceList);
         
+        //Buttons Creation
         button = new JButton("GO");
         button.addActionListener(this);
         
         field = new JTextField();
         
+        //Features of the panel
         add(List, BorderLayout.PAGE_START);
         add(button, BorderLayout.PAGE_END);
         add(field, BorderLayout.CENTER);
@@ -57,16 +69,18 @@ public class Welcome extends JDialog implements ActionListener{
     
     @Override
     public void actionPerformed(ActionEvent e){
-        st = field.getText();
-        n = List.getSelectedIndex();
-        String[] host;
-        host = st.split("\\.");
+        st = field.getText(); //IP Address of the NIC
+        n = List.getSelectedIndex(); //NIC choice of the user
         if (st.length() > 2) {
             this.dispose();
             Thread thread = new Thread() {
                 @Override
                 public void run() {
-                    MainFrame mainFrame = new MainFrame(n,st);
+                    try {
+                        MainFrame mainFrame = new MainFrame(n, st);
+                    } catch (RrdException | IOException | InterruptedException ex) {
+                        Logger.getLogger(Welcome.class.getName()).log(Level.SEVERE, null, ex);
+                    }
                 }
             };
             thread.start();
@@ -74,6 +88,7 @@ public class Welcome extends JDialog implements ActionListener{
     }
     
     public static void main(String[] args) {
+        System.out.print("System is starting...");
         Welcome w = new Welcome();
     }
                 
