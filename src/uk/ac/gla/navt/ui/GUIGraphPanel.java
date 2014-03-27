3023900c337f44ac55bb8835071146487e023006
Mehmet Kemal Deniz <mkdeniz@users.noreply.github.com>
2014-03-27 19:19:11 +0000

    Update GUIGraphPanel.java

--- src/uk/ac/gla/navt/ui/GUIGraphPanel.java
+++ src/uk/ac/gla/navt/ui/GUIGraphPanel.java
@@ -13,26 +13,38 @@ import org.jrobin.core.RrdException;
 import org.jrobin.graph.RrdGraph;
 
 public class GUIGraphPanel extends JPanel {
-	protected RrdGraph graph;
-	protected int width, height;
+	
+    protected RrdGraph graph;
+    protected int width, height;
 
-	public GUIGraphPanel(RrdGraph graph) {
-            this.graph = graph;
-	}
-        
-        @Override
-	public void paintComponent(Graphics g) {
-            try {
-                graph.specifyImageSize(true);
-                graph.renderImage((Graphics2D)g, width, height);
-            }
-            catch (RrdException | IOException ex) {
-            }
-	}
-        
-	public void setGraphDimension(Dimension d) {
-            width = d.width;
-            height = d.height;
-            repaint();
-	}
-}
+    /**
+     * Default Constructor
+     * 
+     * @throws InterruptedException
+     */
+    public GUIGraphPanel(RrdGraph g) {
+        graph = g;
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        try {
+            graph.specifyImageSize(true);
+            graph.renderImage((Graphics2D)g, width, height);
+        }
+        catch (RrdException | IOException ex) {
+        }
+    }
+
+    /**
+     * A method to set the size of the graph as according to 
+     * the size of main frame
+     * 
+     * @param  d size of main frame
+     */
+    public void setGraphDimension(Dimension d) {
+        width = d.width;
+        height = d.height;
+        repaint();
+    }
+    }
